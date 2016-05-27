c4af3966828c8c1cc2d391fd059302bcf4df4b05
sanmen <yanweijin01@163.com>
2016-05-27 23:02:08 +0800

    update

--- src/com/atguigu/mygit/hello.java
+++ src/com/atguigu/mygit/hello.java
@@ -3,6 +3,7 @@ package com.atguigu.mygit;
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("helloworld");
-		System.out.println("service hello"");
+		System.out.println("service hello");
+		System.out.println("local update");
 	}
 }
